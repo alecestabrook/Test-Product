1cf22603dd8a42da0e6a3db1cf56d145d725ee4c
alecestabrook <estabrook@climateadvisers.org>
2023-07-24 12:30:42 -0400

    Initial Commit

---
+++ Test Product/Test_ProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ProductApp.swift
+//  Test Product
+//
+//  Created by Alec Estabrook on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_ProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
